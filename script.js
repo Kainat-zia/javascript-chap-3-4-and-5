ab9fff1b044064f8cfa029fbd56580ce053f71eb
Kainat zia <154695099+Kainat-zia@users.noreply.github.com>
2024-05-22 00:21:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,203 @@
+// chapter 3
+// Question 1
+var age ="your age";
+alert("I am 15 years old");
+
+// Question 2
+var visit = "You have visited this site 20 times ";
+alert(visit);
+document.write("You have visited this site 20 times<br><br>")
+// Question 3
+var birthYear = "My birth year is 2002  Data type of my declared variable is number<br><br>";
+document.write(birthYear)
+
+// Question 4
+var visitorsname = "John doe";
+var producttitle = "5 T shirts";
+var quantity = "how many products a visitor wants to order";
+document.write ("John Doe ordered 5 T-shirts on XYZ Clothing store.<br><br>");
+
+// chapter 4
+// Question 1
+var variable1, variable2, variable3;
+
+// Question 2
+// legal variables
+let firstName;
+let Age;
+let number23;
+let $kainat;
+let _kainat;
+
+// illegal variables
+// let 13;
+// let 14number;
+// let !kainat;
+// let "kainat";
+// let @kainat;
+
+// chapter 5
+// Question 1
+let a =  3;
+let b =  5;
+let sum = a + b;
+console.log ("Sum of " + 3 + " and " + 5 + " is: " + sum)
+document.write("Sum of 3 and 5 is 8<br><br>")
+
+// Question 2
+let c =  3;
+let d =  5;
+let subtraction = c - d;
+console.log ("Subtraction of " + 3 + " and " + 5 + " is: " + subtraction)
+document.write("Subtraction of 3 and 5 is -2<br>")
+
+let e =  3;
+let f =  5;
+let multiplication = e * f;
+console.log ("multiplication of " + 3 + " and " + 5 + " is: " + multiplication)
+document.write("Multiplication of 3 and 5 is 15 <br>")
+
+let g =  3;
+let h =  5;
+let division = g / h;
+console.log ("division of " + 3 + " and " + 5 + " is: " + division)
+document.write("Division of 3 and 5 is 0.6<br>")
+
+let i =  3;
+let j =  5;
+let modulus = i % j;
+console.log ("modulus of " + 3 + " and " + 5 + " is: " + modulus)
+document.write("Modulus of 3 and 5 is 3<br><br>")
+
+// Question 3
+var value = "<br>Value after variable declaration is undefined <br> Initial value:5";
+document.write(value);
+var x = 5
+let y =  ++x;
+document.write("<br>Value of increment is:" + " " + y)
+
+let m = 6;
+let n = 7;
+let add =  6 + 7;  
+document.write("<br>Value after addition is :" + " " + add)
+
+let k = 13
+let L = --k;
+document.write("<br>Value of decrement is:" + " " + L)
+
+// Question 4
+var ticketPrice = 600;
+var buying = 5;
+var  result = 600 * 5;
+console.log("<br><br>Total cost of buy 5 tickets to a movie is " + "  " + result +"PKR");
+document.write("<br>Total cost of buy 5 tickets to a movie is " + "  " + result +"PKR")
+
+// Question 5
+document.write("<br><br>Table of 5")
+var ab = 5;
+var bc = 1;
+var result = 5 * 1;
+document.write("<br> 5x1=" +" "+result)
+var ab = 5;
+var bc = 2;
+var result = 5 * 2;
+document.write("<br> 5x2=" +" "+result)
+var ab = 5;
+var bc = 3;
+var result = 5 * 3;
+document.write("<br> 5x3=" +" "+result)
+var ab = 5;
+var bc = 4;
+var result = 5 * 4;
+document.write("<br> 5x4=" +" "+result)
+var ab = 5;
+var bc = 5;
+var result = 5 * 5;
+document.write("<br> 5x5=" +" "+result)
+var ab = 5;
+var bc = 6;
+var result = 5 * 6;
+document.write("<br> 5x6=" +" "+result)
+var ab = 5;
+var bc = 7;
+var result = 5 * 7;
+document.write("<br> 5x7=" +" "+result)
+var ab = 5;
+var bc = 8;
+var result = 5 * 8;
+document.write("<br> 5x8=" +" "+result)
+var ab = 5;
+var bc = 9;
+var result = 5 * 9;
+document.write("<br> 5x9=" +" "+result)
+var ab = 5;
+var bc = 10;
+var result = 5 * 10;
+document.write("<br> 5x10=" +" "+result)
+
+// question 6
+var temp = 77;
+var result = (77-32)* 5/9;
+console.log(result);
+document.write("<br><br> 25C is 77F")
+document.write("<br>70F is 21.11111111111")
+
+// question 7
+var priceofitem1 = 650;
+var quantityofitem1 = 3 ;
+var prceofitem2 = 100;
+var quantityofitem2 = 7;
+var shippingcharges = 100;
+var result = (650*3 + 100 * 7 + 100 );
+console.log(result);
+document.write("<br><br> Price of item 1 is 650<br> Quantity of item 1 is 3<br>Price of item 2 is 100<br> Quantity of item 2 is 7<br> Shipping charges 100 <br><br>Total cost of your order is" +"  "+result)
+
+
+// Question 8
+var w = 980;
+var x = 804;
+var result = 804 * 100 /980;
+console.log(result);
+document.write("<br><br>Total marks: 980<br> Marks obtained: 804<br> Percentage:"+ "" + result);
+
+
+// Question 9
+var US_dollars = 10;
+var Saud_Riyals = 25;
+var result = ((10 * 104.80) + (25 * 28));
+console.log(result); 
+document.write("<br><br>Total Currency in PKR:"+"  "+result);
+
+// Question 10
+var number = 10;
+var result = ((number + 5) * 10) / 2;
+console.log(result);
+document.write("<br><br>Result is:"+""+ result);
+
+// Question 11
+var current_year = 2024;
+var birth_year = 2002;
+var result = current_year-birth_year;
+console.log(result);
+document.write("<br><br>Current Year:2002<br>Birth year:2002<br>Your age is:"+"  "+result);
+
+// Question 12
+var radius = 20;
+var circumference = 2* 3.142 * radius;
+var result = circumference;
+console.log(result);
+var radius2 = 400;
+var area = 3.142 * radius2 ;
+console.log(area);
+document.write("<br><br> Radius of a circle is:20<br> Tge circumference is "+"  "+result);
+document.write("<br>The area is:"+" "+ area);
+
+// Question 13
+var snack = "favorite snack";
+var currentage = 15;
+var maxage = 65;
+var perday = 3;
+var result =(50-15)
+console.log(result);
+document.write("<br><br>favorite snack:Chocolate chip <br>Current Age: 15<br> Estimated maximum age:65<br> Amount of snack per day: 3<br> you will need 150 chocolate chip to last until the ripe old age of 65")
+
